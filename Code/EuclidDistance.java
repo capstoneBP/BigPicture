df315e59469087e9ba2ddf24178aa58ba50b052f
capstoneBP <capstone.ime@gmail.com>
2017-03-19 18:00:52 +0900

    delete sample.png and modify ED code

--- Code/EuclidDistance.java
+++ Code/EuclidDistance.java
@@ -51,28 +51,28 @@ public class EuclidDistance{
 			j = (int) Double.parseDouble(s);
 			point_array.add(j);
 		}
-
+		
 		String filename = "EuclidDistance_x_y_d.txt";
+		BufferedWriter write_distance = new BufferedWriter(new FileWriter(filename, true));
 		for(int n = 0 ; n <= point_array.size()-2 ; n+=2){
 			double x, y;
 			double next_x, next_y;
 			double dist;
 			x = point_array.get(n);
 			y = point_array.get(n+1);
-
+			
 			for(int m = n+2 ; m < point_array.size() ; m+=2){
 				next_x = point_array.get(m);
 				next_y = point_array.get(m+1);
 				dist = Math.sqrt((next_x-x)*(next_x-x)+(next_y-y)*(next_y-y));
 				dist = (Math.round(dist*100))/100.0;
 				distance.add(dist);
-				BufferedWriter write_distance = new BufferedWriter(new FileWriter(filename, true));
 				write_distance.write((int)x + "," + (int)y + "," + (int)next_x + "," + (int)next_y + "," + dist);
 				write_distance.newLine();
-				write_distance.flush();
-				write_distance.close();
 			}
+			write_distance.flush();
 		}
+		write_distance.close();
 		return distance;
 	}
 }
